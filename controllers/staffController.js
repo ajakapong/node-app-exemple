574a940cc38a8964a786727306894813b285ae01
Apiradee Pattanakijwiwat <maii@salweensolutions.com>
2020-08-14 14:28:56 +0700

    staff

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -17,3 +17,25 @@ exports.search = (req, res, next) => {
         data: { name, age }
     });
 } 
+
+exports.insert = (req, res, next) => {
+    const { name, age } = req.body;
+    return res.status(200).json({
+        data: { name, age }
+    });
+}
+
+exports.update = (req, res, next) => {
+    const { id } = req.params;
+    const { name, age } = req.body;
+    return res.status(200).json({
+        data: { id,name, age }
+    });
+} 
+
+exports.delete = (req, res, next) => {
+    const { id } = req.params;
+    return res.status(200).json({
+        data: { id }
+    });
+} 
--- routes/staff.js
+++ routes/staff.js
@@ -9,4 +9,10 @@ router.get('/', staffController.index);
 
 router.get('/:id', staffController.show);
 
+router.post('/', staffController.insert);
+
+router.put('/:id', staffController.update);
+
+router.delete('/:id', staffController.delete);
+
 module.exports = router;
