fcb812106a241748fd0a45b526a1b4a76d3bd37f
Apiradee Pattanakijwiwat <maii@salweensolutions.com>
2020-08-15 10:17:27 +0700

    staff

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -1,51 +1,71 @@
-const Staff = require('../models/staff')
+const Staff = require('../models/staff');
+const staff = require('../models/staff');
 
 
-exports.index = (req, res, next) => {
-    res.send('respond with a resource');
+exports.index = async (req, res, next) => {
+    // const staff = await Staff.find().select('-sarary').sort('-_id');
+    const staff = await Staff.find().where('salary').gte(20000).sort('-_id');
+    return res.status(200).json({
+        data: staff
+    })
 }
 
-exports.show = (req, res, next) => {
+exports.show = async (req, res, next) => {
     const { id } = req.params;
-    console.log(id);
+    const staff = await Staff.findById(id);
     return res.status(200).json({
-        data: id
+        data: staff
     });
 }
 
-exports.search = (req, res, next) => {
-    const { name, age } = req.query;
+exports.search = async (req, res, next) => {
+    const { name } = req.query;
+    const staff = await Staff.find({name: { $regex: '.*' + name + '.*', $options: 'i' } })
+
     return res.status(200).json({
-        data: { name, age }
+        data: staff
     });
-} 
+}
 
 exports.insert = async (req, res, next) => {
     const { name, salary } = req.body;
 
     const staff = new Staff({
-        name : name,
+        name: name,
         salary: salary
     })
-   
-    await staff.save();
+
+    const dataUpdate = await staff.save();
 
     return res.status(201).json({
-        message: 'เพิ่มข้อมูลเรียบร้อย'
+        message: 'เพิ่มข้อมูลเรียบร้อย',
+        data: dataUpdate
     });
 }
 
-exports.update = (req, res, next) => {
+exports.update = async (req, res, next) => {
     const { id } = req.params;
-    const { name, age } = req.body;
-    return res.status(200).json({
-        data: { id,name, age }
-    });
-} 
+    const { name, salary } = req.body;
 
-exports.delete = (req, res, next) => {
-    const { id } = req.params;
+    const staff = await Staff.updateOne({ _id: id },{
+        name : name,
+        salary:salary
+    });
+
     return res.status(200).json({
-        data: { id }
+        staff : staff,
+        message: "แก้ไข้อมูลเรียบร้อย"
+    });
+}
+
+exports.delete = async (req, res, next) => {
+
+    const { id } = req.params;
+
+    const staff = await Staff.deleteOne({ _id: id });
+
+    return res.status(200).json({
+        staff: staff,
+        data: 'ลบข้อมูลเรียบร้อยแล้ว'
     });
 } 
--- models/staff.js
+++ models/staff.js
@@ -2,16 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    name: {
-        type: String,
-        trim: true, required: true
-    },
+    name: { type: String, trim: true, required: true },
     salary: { type: Number },
     created_at: { type: Date, default: Date.now }
-}, {
-    collation: 'staffs'
-});
+  },{
+      collection: 'staffs'
+  });
+  
 
-const staff = mongoose.model('Staff', schema);
+  const staff = mongoose.model('Staff', schema);
 
 module.exports = staff;
